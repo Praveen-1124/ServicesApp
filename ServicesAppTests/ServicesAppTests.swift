7747b272fed2c14ef89624b48fd992bd0c15774f
praveen2498 <praveen@pepul.com>
2026-03-07 15:48:08 +0530

    Initial Commit

---
+++ ServicesAppTests/ServicesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesAppTests.swift
+//  ServicesAppTests
+//
+//  Created by Praveen A on 06/03/26.
+//
+
+import Testing
+@testable import ServicesApp
+
+struct ServicesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
